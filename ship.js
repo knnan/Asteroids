3c58f3444778be14daad6089a5f0611366e36ea3
knnan <28503456+knnan@users.noreply.github.com>
2019-07-02 09:37:49 +0530

    added local  p5 copy

--- ship.js
+++ ship.js
@@ -4,74 +4,74 @@
 // Code for: https://youtu.be/hacZU523FyM
 function preload()
 {
-  jet = loadImage("jets/jet2.png");
+	jet = loadImage("jets/jet4.png");
 }
 function Ship() {
-  this.pos = createVector(width / 2, height / 2);
-  this.r = 30;
-  this.heading = 0;
-  this.rotation = 0;
-  this.vel = createVector(0, 0);
-  this.isBoosting = false;
+	this.pos = createVector(width / 2, height / 2);
+	this.r = 30;
+	this.heading = 0;
+	this.rotation = 0;
+	this.vel = createVector(0, 0);
+	this.isBoosting = false;
 
-  this.boosting = function(b) {
-    this.isBoosting = b;
-  }
+	this.boosting = function(b) {
+		this.isBoosting = b;
+	}
 
-  this.update = function() {
-    if (this.isBoosting) {
-      this.boost();
-    }
-    this.pos.add(this.vel);
-    this.vel.mult(0.99);
-  }
+	this.update = function() {
+		if (this.isBoosting) {
+			this.boost();
+		}
+		this.pos.add(this.vel);
+		this.vel.mult(0.99);
+	}
 
-  this.boost = function() {
-    var force = p5.Vector.fromAngle(this.heading);
-    force.mult(0.1);
-    this.vel.add(force);
-  }
+	this.boost = function() {
+		var force = p5.Vector.fromAngle(this.heading);
+		force.mult(0.1);
+		this.vel.add(force);
+	}
 
-  this.hits = function(asteroid) {
-    var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
-    if (d < this.r + asteroid.r) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+	this.hits = function(asteroid) {
+		var d = dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y);
+		if (d < this.r + asteroid.r) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-  this.render = function() {
-    push();
-    translate(this.pos.x, this.pos.y);
-    rotate(this.heading + PI / 2);
-    // fill(108,194,74);
-    fill(0,255,0);
-    stroke(255);
-    image(jet,-this.r,-this.r,75,75);
-    // triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
-    pop();
-  }
+	this.render = function() {
+		push();
+		translate(this.pos.x, this.pos.y);
+		rotate(this.heading + PI / 2);
+		// fill(108,194,74);
+		fill(0,255,0);
+		stroke(255);
+		image(jet,-this.r-9,-this.r,75,75);
+		// triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
+		pop();
+	}
 
-  this.edges = function() {
-    if (this.pos.x > width + this.r) {
-      this.pos.x = -this.r;
-    } else if (this.pos.x < -this.r) {
-      this.pos.x = width + this.r;
-    }
-    if (this.pos.y > height + this.r) {
-      this.pos.y = -this.r;
-    } else if (this.pos.y < -this.r) {
-      this.pos.y = height + this.r;
-    }
-  }
+	this.edges = function() {
+		if (this.pos.x > width + this.r) {
+			this.pos.x = -this.r;
+		} else if (this.pos.x < -this.r) {
+			this.pos.x = width + this.r;
+		}
+		if (this.pos.y > height + this.r) {
+			this.pos.y = -this.r;
+		} else if (this.pos.y < -this.r) {
+			this.pos.y = height + this.r;
+		}
+	}
 
-  this.setRotation = function(a) {
-    this.rotation = a;
-  }
+	this.setRotation = function(a) {
+		this.rotation = a;
+	}
 
-  this.turn = function() {
-    this.heading += this.rotation;
-  }
+	this.turn = function() {
+		this.heading += this.rotation;
+	}
 
 }
